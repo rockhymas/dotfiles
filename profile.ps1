60410f52c25489c9972901b4be9798ec9ab65f1f
Rock Hymas <rock@fogcreek.com>
2009-09-26 01:47:43 +0400

    Fix profile script in case VS isn't installed

--- profile.ps1
+++ profile.ps1
@@ -9,15 +9,19 @@ function Get-Batchfile ($file) {
 function VsVars32($version = "9.0")
 {
     $key = "HKLM:SOFTWARE\Microsoft\VisualStudio\" + $version
-    if ((get-itemproperty $key).InstallDir -eq $null) {
-        $key = "HKLM:SOFTWARE\Wow6432Node\Microsoft\VisualStudio\" + $version
+    if ((Test-Path $key) -eq $true) {
+    	if ((get-itemproperty $key).InstallDir -eq $null) {
+        	$key = "HKLM:SOFTWARE\Wow6432Node\Microsoft\VisualStudio\" + $version
+    	}
+        if ((Test-Path $key) -eq $true) {
+	        $VsKey = get-ItemProperty $key
+            $VsInstallPath = [System.IO.Path]::GetDirectoryName($VsKey.InstallDir)
+            $VsToolsDir = [System.IO.Path]::GetDirectoryName($VsInstallPath)
+            $VsToolsDir = [System.IO.Path]::Combine($VsToolsDir, "Tools")
+            $BatchFile = [System.IO.Path]::Combine($VsToolsDir, "vsvars32.bat")
+            Get-Batchfile $BatchFile
+        }
     }
-    $VsKey = get-ItemProperty $key
-    $VsInstallPath = [System.IO.Path]::GetDirectoryName($VsKey.InstallDir)
-    $VsToolsDir = [System.IO.Path]::GetDirectoryName($VsInstallPath)
-    $VsToolsDir = [System.IO.Path]::Combine($VsToolsDir, "Tools")
-    $BatchFile = [System.IO.Path]::Combine($VsToolsDir, "vsvars32.bat")
-    Get-Batchfile $BatchFile
 }
 
 function prompt
