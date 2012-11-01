6c1297175cc8aba452d7ed700d67c33afdcaad7c
Rock Hymas <rhymas@trackabout.com>
2012-11-02 05:07:59 +0600

    Create autohotkey shortcut to run automatically on startup

--- setup/setuptools.ps1
+++ setup/setuptools.ps1
@@ -12,3 +12,12 @@ if (!(Test-Path $profile)) {
 
 reg import "$(Split-Path $myinvocation.mycommand.path)\consolasfont.reg"
 reg import "$(Split-Path $myinvocation.mycommand.path)\capslockisctrl.reg"
+
+$ahklnk = $env:USERPROFILE + "\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\.ahk.lnk"
+if (!(Test-Path $ahklnk))
+{
+    $objShell = New-Object -ComObject ("WScript.Shell")
+    $objShortCut = $objShell.CreateShortcut($ahklnk)
+    $objShortCut.TargetPath = $env:USERPROFILE + ".ahk"
+    $objShortCut.Save()
+}
