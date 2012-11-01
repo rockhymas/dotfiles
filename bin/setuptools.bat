94cabed9396371801868da72490cbbec038ddd57
Rock Hymas <rhymas@trackabout.com>
2012-11-02 03:53:32 +0600

    Strip it down to the stuff that I know works (with perms)

--- bin/setuptools.bat
+++ bin/setuptools.bat
@@ -1,3 +1,2 @@
-elevate-process servermanagercmd -install PowerShell
-elevate-process powershell -c "Set-ExecutionPolicy RemoteSigned"
+powershell -c "Set-ExecutionPolicy RemoteSigned"
 powershell -c "%~dp0\setuptools.ps1"
--- bin/setuptools.ps1
+++ bin/setuptools.ps1
@@ -1,23 +1,14 @@
-function elevate-process
-{
-    $file, [string]$arguments = $args;
-    $psi = new-object System.Diagnostics.ProcessStartInfo $file;
-    $psi.Arguments = $arguments;
-    $psi.Verb = "runas";
-    $psi.WorkingDirectory = get-location;
-    [System.Diagnostics.Process]::Start($psi);
-}
-
 if ($(Get-ExecutionPolicy) -ne "RemoteSigned") {
-    elevate-process powershell -c "Set-ExecutionPolicy RemoteSigned"
+    Set-ExecutionPolicy RemoteSigned
 }
-$profilepath = (new-object System.IO.FileInfo $profile).Directory.FullName
-if (!(Test-Path $profilepath)) {
-    New-Item (new-object System.IO.FileInfo $profile).Directory.FullName -type directory
+
+if (!(Test-Path $profile)) {
+    $profilepath = Split-Path $profile
+    if (!(Test-Path $profilepath)) {
+        New-Item $profilepath -type directory
+    }
+    echo ". `$env:userprofile\.ps1" > $profile
 }
-Copy-Item $env:userprofile\bin\profileredirect.ps1 $profile
 
-& ".\consolasfont.reg"
-& ".\capslockisctrl.reg"
-
-[Diagnostics.Process]::Start("http://www.vim.org/download.php#pc")
+reg import "$(Split-Path $myinvocation.mycommand.path)\consolasfont.reg"
+reg import "$(Split-Path $myinvocation.mycommand.path)\capslockisctrl.reg"
